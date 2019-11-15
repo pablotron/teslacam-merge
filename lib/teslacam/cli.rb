541b46bd81bfcfb2fd6e518c09990144887a988e
Paul Duncan <pabs@pablotron.org>
2019-11-14 20:24:16 -0500

    add comment

--- lib/teslacam/cli.rb
+++ lib/teslacam/cli.rb
@@ -7,6 +7,9 @@ module TeslaCam::CLI
   LIB_DIR = File.join(__dir__, 'cli').freeze
   autoload :Config, File.join(LIB_DIR, 'config.rb')
 
+  #
+  # Run from command-line.
+  #
   def self.run(app, args)
     # get config from command-line, build model
     config = ::TeslaCam::CLI::Config.new(app, args)
