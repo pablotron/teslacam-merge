54b74c29b39e37e2b8786ec4392417389f477b4c
Paul Duncan <pabs@pablotron.org>
2019-11-15 07:53:44 -0500

    refactor filter

--- lib/teslacam/filter.rb
+++ lib/teslacam/filter.rb
@@ -2,42 +2,19 @@ require 'time'
 
 class TeslaCam::Filter
   def initialize(model)
+    config = model.config
     num_times = model.times.size
-
-    w = model.config.size.w
-    h = model.config.size.w
-    font = get_font(model.config)
 
     # build filter clause
     @s = model.times.each_with_index.map { |time, i|
-      # extract timestamp from key
-      ts = Time.parse(time)
-
-      # build sink id
-      sink = (num_times > 1) ? "[v#{i}]" : ''
-
-      "
-        nullsrc=size=#{w*2}x#{h*2}:duration=60 [v#{i}_bg];
-        [#{4 * i + 0}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_tl];
-        [#{4 * i + 1}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_tr];
-        [#{4 * i + 2}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_bl];
-        [#{4 * i + 3}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_br];
-        [v#{i}_bg][v#{i}_tl] overlay=shortest=0 [v#{i}_t0];
-        [v#{i}_t0][v#{i}_tr] overlay=shortest=0:x=#{w} [v#{i}_t1];
-        [v#{i}_t1][v#{i}_bl] overlay=shortest=0:y=#{h} [v#{i}_t2];
-        [v#{i}_t2][v#{i}_br] overlay=shortest=0:x=#{w}:y=#{h} [v#{i}_t3];
-        [v#{i}_t3] drawtext=text=front:x=4:y=3:#{font} [v#{i}_t4];
-        [v#{i}_t4] drawtext=text=back:x=(w-text_w-4):y=3:#{font} [v#{i}_t5];
-        [v#{i}_t5] drawtext=text=left:x=4:y=(h-text_h-3):#{font} [v#{i}_t6];
-        [v#{i}_t6] drawtext=text=right:x=(w-text_w-4):y=(h-text_h-3):#{font} [v#{i}_t7];
-        [v#{i}_t7] drawtext=text=%{pts\\\\:localtime\\\\:#{ts.to_i}}:x=(w-text_w)/2:y=(h-text_h-5):#{font} #{sink}
-      "
-    }.concat((num_times > 1) ? [
-      '%s concat=n=%d' % [
-        num_times.times.map { |i| "[v#{i}]" }.join(''),
-        num_times,
-      ],
-    ] : []).join(';').freeze
+      [
+        sources(i, config),
+        overlays(i, config),
+        texts(i, config, time, num_times),
+      ]
+    }.flatten.concat(
+      concat_expr(num_times)
+    ).join(';').freeze
   end
 
   def to_s
@@ -45,6 +22,85 @@ class TeslaCam::Filter
   end
 
   private
+
+  #
+  # Get video sources.
+  #
+  def sources(i, config)
+    w = config.size.w
+    h = config.size.h
+
+    "
+      nullsrc=size=#{w*2}x#{h*2}:duration=60 [v#{i}_bg];
+      [#{4 * i + 0}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_tl];
+      [#{4 * i + 1}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_tr];
+      [#{4 * i + 2}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_bl];
+      [#{4 * i + 3}:v] setpts=PTS-STARTPTS, scale=#{w}x#{h} [v#{i}_br]
+    "
+  end
+
+  #
+  # Get video overlays.
+  #
+  def overlays(i, config)
+    w = config.size.w
+    h = config.size.h
+
+    "
+      [v#{i}_bg][v#{i}_tl] overlay=shortest=0 [v#{i}_t0];
+      [v#{i}_t0][v#{i}_tr] overlay=shortest=0:x=#{w} [v#{i}_t1];
+      [v#{i}_t1][v#{i}_bl] overlay=shortest=0:y=#{h} [v#{i}_t2];
+      [v#{i}_t2][v#{i}_br] overlay=shortest=0:x=#{w}:y=#{h} [v#{i}_t3]
+    "
+  end
+
+  #
+  # Text overlays.
+  #
+  TEXTS = [{
+    text: 'front',
+    x: '4',
+    y: '3',
+  }, {
+    text: 'back',
+    x: '(w-text_w-4)',
+    y: '3',
+  }, {
+    text: 'left',
+    x: '4',
+    y: '(h-text_h-3)',
+  }, {
+    text: 'right',
+    x:    '(w-text_w-4)',
+    y:    '(h-text_h-3)',
+  }, {
+    text: '%%{pts\\\\:localtime\\\\:%<ts>i}',
+    x:    '(w-text_w)/2',
+    y:    '(h-text_h-5)',
+  }]
+
+  #
+  # Get text overlays
+  #
+  def texts(i, config, time, num_times)
+    # get timestamp offset and font
+    ts = Time.parse(time).to_i
+    font = get_font(config)
+
+    # build and return result
+    '[v%<i>d_t3] %<texts>s %<sink>s' % {
+      i: i,
+
+      texts: TEXTS.map { |row|
+        'drawtext=text=%<text>s:x=%<x>s:y=%<y>s:%<font>s' % row.merge({
+          text: row[:text] % { ts: ts },
+          font: font,
+        })
+      }.join(', '),
+
+      sink: (num_times > 1) ? "[v#{i}]" : ''
+    }
+  end
 
   #
   # Get font config
@@ -65,5 +121,17 @@ class TeslaCam::Filter
       'borderw=1',
       'bordercolor=black@0.5',
     ].join(':')
+  end
+
+  #
+  # Get concat statement.
+  #
+  def concat_expr(num_times)
+    (num_times > 1) ? [
+      '%s concat=n=%d' % [
+        num_times.times.map { |i| "[v#{i}]" }.join(''),
+        num_times,
+      ],
+    ] : []
   end
 end
